af973c0f82aff43273a320f6cba47e1a9651978f
sureshemerlin <35285202+sureshemerlin@users.noreply.github.com>
2020-01-14 22:46:13 -0500

    ruru

---
+++ src/test/java/com.java.test/SampleTest.java
@@ -0,0 +1,18 @@
+package com.java.test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SampleTest {
+
+    @Test
+    public void multiplicationOfZeroIntegersShouldReturnZerotest() {
+        SampleTest tester = new SampleTest(); // MyClass is tested
+
+        // assert statements
+        assertEquals(0, 0);
+        assertEquals(0, 0);
+        assertEquals(0, 0);
+    }
+}
